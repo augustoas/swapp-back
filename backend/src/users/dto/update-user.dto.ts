4baa3a7e8e766752cebca8459cc707ae8c8b4898
Rodrigo Ignacio Zenteno Smith <rzenteno@mmlv.cl>
2023-08-08 12:30:26 -0400

    update user DTO

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,24 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString, IsOptional, IsDateString, Matches, IsEmail, IsEnum } from 'class-validator';
+import { Gender } from '../../types/Gender.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+// defines the shape of the data required for authentication
+export class UpdateUserDto {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEmail()
+  @Matches(/^.+@lovalledor.cl$/, {message: 'email must end in @lovalledor.cl.'})
+  readonly email: string;
+
+  @IsEnum(Gender)
+  @IsOptional()
+  readonly gender: Gender;
+
+  @IsDateString()
+  @IsOptional()
+  birthdate: Date;
+}
