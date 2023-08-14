6efec7388f162d59443ea878d54d7e3a97091951
Rodrigo Ignacio Zenteno Smith <rzenteno@mmlv.cl>
2023-08-14 15:38:55 -0400

    generate token, save token in db & reset password logic

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,11 +6,13 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../database/entities/user.entity';
 import { Repository } from 'typeorm';
-import { SignUpDto } from './dto/signup.dto';
-import { SignInDto } from './dto/signin.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { LoginResponse, UserWithoutSensitiveData } from '../types/Auth.types';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class AuthService {
@@ -74,5 +76,57 @@ export class AuthService {
       message: 'User information from google',
       user: req.user,
     };
+  }
+
+  async generateResetToken(email: string): Promise<{status: boolean, message: string}> {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return {
+        status: false,
+        message: 'User not found'
+      }
+    }
+
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    user.resetToken = resetToken;
+    user.resetTokenExpiration = new Date(Date.now() + 3600000); // Token expires in 1 hour
+    await this.userRepository.save(user);
+
+    return {
+      status: true,
+      message: 'Reset token sent successfully'
+    }
+  }
+
+  async resetPassword(resetPasswordDto: ResetPasswordDto): Promise<{status: boolean, message: string}> {
+    const { resetToken, newPassword, confirmPassword } = resetPasswordDto;
+    const user = await this.userRepository.findOne({ where: { resetToken } });
+
+    if (!user || user.resetTokenExpiration <= new Date()) {
+      return {
+        status: false,
+        message: 'Invalid or expired reset token'
+      }
+    }
+    if (newPassword !== confirmPassword) {
+      return {
+        status: false,
+        message: 'New password and confirmed password must be equal'
+      }
+    }
+
+    // Update the user's password
+    user.password = newPassword;
+    user.resetToken = null;
+    user.resetTokenExpiration = null;
+    await this.userRepository.save(user);
+
+    // Optionally, log the user in after resetting the password
+    // This depends on your application's requirements
+    return {
+      status: true,
+      message: 'Password reset successful'
+    }
   }
 }
