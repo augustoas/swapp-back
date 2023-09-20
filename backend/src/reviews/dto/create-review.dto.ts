f897541044908e1f6c91705cec364d639a2e9f08
Ignacio Zenteno Smith <izenteno22@gmail.com>
2023-09-20 16:14:57 -0300

    reviews resource REST api

--- backend/src/reviews/dto/create-review.dto.ts
+++ backend/src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,25 @@
-export class CreateReviewDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsNumberString,
+} from 'class-validator';
+
+// defines the shape of the data required
+export class CreateReviewDto {
+  @IsNotEmpty()
+  @IsString()
+  text: string;
+
+  @IsNotEmpty()
+  @IsString()
+  rating: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  reviewReceiverId: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  jobId: string;
+}
--- backend/src/reviews/reviews.service.ts
+++ backend/src/reviews/reviews.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Review } from 'src/database/entities/review.entity';
+import { User } from 'src/database/entities/user.entity';
+import { Job } from 'src/database/entities/job.entity';
 
 @Injectable()
 export class ReviewsService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+  ) { }
+
+  async create(createReviewDto: CreateReviewDto, user: User): Promise<Review> {
+    const { jobId, reviewReceiverId, ...createReviewData } = createReviewDto
+    const newReview = this.reviewRepository.create(createReviewData);
+    newReview.job = { id: +jobId } as Job;
+    newReview.reviewCreator = { id: +user.id } as User;
+    newReview.reviewReceiver = { id: +reviewReceiverId } as User;
+    return this.reviewRepository.save(newReview);
   }
 
-  findAll() {
-    return `This action returns all reviews`;
+  async findAll(): Promise<Array<Review>> {
+    return this.reviewRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  async findOne(id: number): Promise<Review> {
+    return this.reviewRepository.findOne({ where: { id: id } });
   }
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
+  async update(id: number, updateReviewDto: UpdateReviewDto): Promise<Review> {
+    const thisReview = await this.reviewRepository.findOne({ where: { id: id } });
+    if (updateReviewDto.jobId)
+      thisReview.job = { id: +updateReviewDto.jobId } as Job;
+    if (updateReviewDto.reviewReceiverId)
+      thisReview.reviewReceiver = { id: +updateReviewDto.reviewReceiverId } as User;
+
+    await this.reviewRepository.update(id, thisReview);
+    return this.reviewRepository.findOne({ where: { id: id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  async remove(id: number): Promise<void> {
+    await this.reviewRepository.delete(id);
   }
 }
