0d85365bcd14f4dc0492f22be79b51c523ecfc57
Ignacio Zenteno Smith <izenteno22@gmail.com>
2023-10-13 15:17:01 -0300

    fix url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ResponseInterceptor } from './helpers/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('backend'); // This adds /backend to all routes
   app.enableCors(); // Enable CORS for all routes
   // Alternatively, you can set CORS options explicitly:
   // app.enableCors({
